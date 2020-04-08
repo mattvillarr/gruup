55af5cf473acb094ceb809fb96179a2e4e8d916d
Matt <mattvillarr@gmail.com>
2020-04-07 19:14:23 -0500

    added sandbox message args

--- bot.js
+++ bot.js
@@ -2,12 +2,29 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 const { token } = require("./config.json");
 
-bot.on("ready", () => {
+const PREFIX = "!";
+
+bot.on('ready', () => {
   console.log("bot is gtg!");
 });
 
-bot.on("message", (msg) => {
-  if (msg.content === "HELLO") msg.reply("SUH DUDE");
+bot.on('message', (msg) => {
+  let args = msg.content.substring(PREFIX.length).split(" ");
+
+  switch (args[0]) {
+    case 'help':
+      msg.channel.send('SUH DUDE');
+      break;
+
+    case 'about':
+      msg.channel.send('LINK TO COOL WEBSITE HERE');
+      break;
+      
+    case 'clear':
+        if(!args[1]) return msg.reply('ERROR! `clear` requires a second numerical parameter');
+        msg.channel.bulkDelete(args[1]);
+        break;
+  }
 });
 
 bot.login(token);
