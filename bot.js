bbcd78c8c3edb5e10f522489ea8837bc53c1e2bb
Matt <mattvillarr@gmail.com>
2020-04-09 00:12:25 -0500

    added cooldown

--- bot.js
+++ bot.js
@@ -3,12 +3,14 @@ const fs = require('fs');
 const { prefix, token } = require('./config.json');
 
 const bot = new Discord.Client();
+
+const cooldowns = new Discord.Collection();
 bot.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
 
 for (const f of commandFiles) {
-  const cmd = require(`./commands/${f}`);
-  bot.commands.set(cmd.name, cmd);
+  const command = require(`./commands/${f}`);
+  bot.commands.set(command.name, command);
 }
 
 bot.on('ready', () => {
@@ -23,6 +25,30 @@ bot.on('message', (msg) => {
   console.log('cmd: ', cmd, '\nargs: ', args);
 
   if (!bot.commands.has(cmd)) return;
+
+  if (!cooldowns.has(cmd.name)) {
+    cooldowns.set(cmd.name, new Discord.Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(cmd.name);
+  const cooldownAmount = (cmd.cooldown || 5) * 1000;
+
+  if (timestamps.has(msg.author.id)) {
+    const expirationTime = timestamps.get(msg.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return msg.reply(
+        `please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${
+          cmd.name
+        }\` command.`
+      );
+    }
+  }
+
+  timestamps.set(msg.author.id, now);
+  setTimeout(() => timestamps.delete(msg.author.id), cooldownAmount);
 
   try {
     bot.commands.get(cmd).execute(msg, args);
--- commands/poll.js
+++ commands/poll.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 module.exports = {
   name: 'poll',
   description: 'lets users vote y/n, or on specific emotes',
+  cooldown: 10, // 10 second cooldown
   execute(msg, args) {
     const e = new Discord.MessageEmbed()
       .setColor(0xffc300)
